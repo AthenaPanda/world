20f701cb38f3dbae02b730f6bfd7743dc694d0f8
Maple <31100207+AthenaPanda@users.noreply.github.com>
2019-12-29 14:36:10 +0800

    add dict methods

--- Tone.py
+++ Tone.py
@@ -1,1 +1,16 @@
+#dict sample
+seq = ('name', 'age', 'class')
+dict1 = dict.fromkeys(seq, ('Tom', 23, 3))
+print(str(dict1))
 
+dict2 = {"age": 20, "name": "Wind", "pro": "busin"}
+print(dict2.items())
+print(dict2.keys())
+print(dict2.values())
+if 'age' in dict1.keys():
+    print("find the age!")
+
+dict3 = {"A":30, "B":65, "C":89, "D":80}
+for key,value in dict3.items():
+    if value > 75:
+        print(key)
